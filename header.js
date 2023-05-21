040163370ca61d9d51a3417b348ddc812fbed31e
Emma5322 <131373067+Emma5322@users.noreply.github.com>
2023-05-21 18:51:38 -0300

    Add files via upload

--- header.js
+++ header.js
@@ -19,7 +19,7 @@ document.write("</div>");
 document.write("<div class='botonav navbar'>");
 document.write("<div class='buscaTuVuelo'><a href='index.html'>Busca tu vuelo</a></div>");
 document.write("<div class='promociones'><a href='promociones.html'>Promociones</a></div>");
-document.write("<div class='clubAuri'><a href='clubAuri.html'>Club Auri</a></div>");
+document.write("<div class='clubAuri'><a href='clubauri.html'>Club Auri</a></div>");
 document.write("<div class='actividades'><a href='actividades.html'>Actividades</a></div>");
 document.write("</div>");
 document.close();
