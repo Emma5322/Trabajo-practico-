86c9dd52b3669e856a657d38cfda51998f78d93f
Emma5322 <131373067+Emma5322@users.noreply.github.com>
2023-05-21 18:09:33 -0300

    actualización para la web

---
+++ validacionrecuperarclave.js
@@ -0,0 +1,19 @@
+function validarFormulariorecupero() {
+    var dni = document.getElementById("dni").value.trim();
+
+    if (dni === "") {
+        alert("Por favor, complete el campo vacío.");
+        return false;
+    }
+
+    // Verificar si el DNI contiene solo 8 dígitos numéricos
+    if (dni.length !== 8) {
+        alert("El campo 'dni' debe contener exactamente 8 dígitos numéricos.");
+        return false;
+
+    }
+
+    // Si todas las validaciones son exitosas, enviar el formulario
+    alert("Formulario enviado correctamente.");
+    return true;
+}
