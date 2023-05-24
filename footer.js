b555b4cbd2abca1dd3cdaf71601445168c2d9691
Emma5322 <131373067+Emma5322@users.noreply.github.com>
2023-05-24 14:11:00 -0300

    Add files via upload

--- footer.js
+++ footer.js
@@ -6,11 +6,12 @@ document.write("        <p> Domicilio legal Santa Fe 100 - CABA </p>");
 document.write("        <p>Telefono de atención Tel: 0800-AURI-1234</p>");
     
 document.write("</div>");
+document.write("<div class='mapa'>");
+document.write("    <iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1642.2881678507083!2d-58.4101086!3d-34.5895851!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca9d49ea93ed%3A0x67ccd474aea91eeb!2sAv.%20Sta.%20Fe%2C%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1684880695385!5m2!1ses!2sar'></iframe>");
+document.write("</div>");
 document.write("<div>");
 document.write("    <p>Seguinos</p>");
 document.write("");
-document.write("    <iframe src='https://goo.gl/maps/tnMikrFaHrmPd6J59?coh=178571&entry=tt' frameborder='0'></iframe>");
-
 document.write("    <a href='https://www.facebook.com'><img src='Images/facebook.PNG' alt=''></a>");
 document.write("    <a href='https://www.instagram.com/'><img src='Images/instagram.PNG' alt=''></a>");
 document.write("    <a href='https://www.tiktok.com'><img src='Images/tiktok.PNG' alt=''></a>");
